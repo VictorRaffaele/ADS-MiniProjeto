41820d05729819c7a32678481eeca6e795e85368
victor <victorraffaele@gmail.com>
2021-10-07 17:54:05 -0300

    retirada da pausa do nench e implementaçao do script para monitoramento dos recursos de rede

--- miniprojeto.sh
+++ miniprojeto.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+echo "amostra RX TX recebido enviado tempo" > log_rede.txt
+amostra=0
+
+while [ TRUE ]
+do
+
+rx=`/sbin/ifconfig "ens33" | grep "RX" | grep "bytes" | awk '{print $5}'`
+
+tx=`/sbin/ifconfig "ens33" | grep "TX" | grep "bytes" | awk '{print $5}'`
+
+recebido=`ifstat | awk '{print $0}'`
+
+enviado=`ifstat | awk '{print $1}'`
+
+data=`date`
+
+amostra=`expr $amostra + 1`
+
+echo $amostra $rx $tx $recebido $enviado $data >> log_rede.txt
+
+done
