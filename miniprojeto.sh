5c6ae57608c1507ccb344cb7f1b416dd47af5925
victor <victorraffaele@gmail.com>
2021-10-08 11:36:25 -0300

    Script de monitoramento pronto

--- miniprojeto.sh
+++ miniprojeto.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-echo "amostra RX TX recebido enviado tempo" > log_rede.txt
+echo "amostra interface recebido pacote_rec enviado pacote_env tempo" > log_rede.txt
 amostra=0
 
 while [ TRUE ]
 do
 
-rx=`/sbin/ifconfig "ens33" | grep "RX" | grep "bytes" | awk '{print $5}'`
+rec_local=`cat /proc/net/dev | grep "lo" | awk '{print $2}'`
+rec_pack_lo=`cat /proc/net/dev | grep "lo" | awk '{print $3}'`
 
-tx=`/sbin/ifconfig "ens33" | grep "TX" | grep "bytes" | awk '{print $5}'`
+send_local=`cat /proc/net/dev | grep "lo" | awk '{print $10}'`
+send_pack_lo=`cat /proc/net/dev | grep "lo" | awk '{print $11}'`
 
-recebido=`ifstat | awk '{print $0}'`
+rec_ens33=`cat /proc/net/dev | grep "ens33" | awk '{print $2}'`
+rec_pack_ens33=`cat /proc/net/dev | grep "ens33" | awk '{print $3}'`
 
-enviado=`ifstat | awk '{print $1}'`
+send_ens33=`cat /proc/net/dev | grep "ens33" | awk '{print $10}'`
+send_pack_ens33=`cat /proc/net/dev | grep "ens33" | awk '{print $11}'`
 
 data=`date`
 
 amostra=`expr $amostra + 1`
 
-echo $amostra $rx $tx $recebido $enviado $data >> log_rede.txt
-
+echo $amostra "      lo:      " $rec_local "  " $rec_pack_lo "      " $send_local " " $send_pack_lo "      " $data >> log_rede.txt
+echo "        ens33:   " $rec_ens33 " " $rec_pack_ens33 "    " $send_ens33 "" $send_pack_ens33 >> log_rede.txt
+sleep 15
 done
